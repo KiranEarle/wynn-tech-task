51fc137031d392b1aeb82241c99d56ed53b1da1f
KiranEarle <earlekiran@gmail.com>
2025-05-30 00:16:10 +0100

    started mobile nav bar

--- src/app/components/molecules/NavBar/NavBar.tsx
+++ src/app/components/molecules/NavBar/NavBar.tsx
@@ -17,35 +17,43 @@ export type NavBarProps = {
 
 const NavBar = ({ logo, navList, locals }: NavBarProps) => {
   return (
-    <div className="Nav">
-      <div className="Nav-container">
+    <>
+      <div className="Nav">
+        <div className="Nav-container">
+          <a className="Nav-logo-link" href={logo.href}>
+            <img src={logo.src} alt={logo.alt} className="Nav-logo" />
+          </a>
+          <nav>
+            <ol className="Nav-list">
+              {navList?.map((item) => {
+                return (
+                  <li className="Nav-list-item" key={item.label}>
+                    <a href={item.href}>{item.label?.toUpperCase()}</a>
+                  </li>
+                );
+              })}
+            </ol>
+          </nav>
+          <div className="Nav-local-container">
+            <select className="Nav-local-select">
+              {locals?.map((item) => {
+                return (
+                  <option key={item.label}>
+                    {item.label?.toLocaleUpperCase()}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="Nav-mobile">
         <a className="Nav-logo-link" href={logo.href}>
           <img src={logo.src} alt={logo.alt} className="Nav-logo" />
         </a>
-        <nav>
-          <ol className="Nav-list">
-            {navList?.map((item) => {
-              return (
-                <li className="Nav-list-item" key={item.label}>
-                  <a href={item.href}>{item.label?.toUpperCase()}</a>
-                </li>
-              );
-            })}
-          </ol>
-        </nav>
-        <div className="Nav-local-container">
-          <select className="Nav-local-select">
-            {locals?.map((item) => {
-              return (
-                <option key={item.label}>
-                  {item.label?.toLocaleUpperCase()}
-                </option>
-              );
-            })}
-          </select>
-        </div>
+        <div className="Nav-mobile-hamburger"></div>
       </div>
-    </div>
+    </>
   );
 };
 
