f1310b93690b5f2cb0de96d0c8c71977d936e754
KiranEarle <earlekiran@gmail.com>
2025-06-01 13:13:56 +0100

    phone number field done

--- src/app/components/atoms/PhoneNumberInput/PhoneNumberInput.tsx
+++ src/app/components/atoms/PhoneNumberInput/PhoneNumberInput.tsx
@@ -7,6 +7,9 @@ import phoneNumberLocals from "@resources/phoneNumberLocals";
 
 import WynnRegistrationsApp from "@app-types/WynnRegistrationsApp.types";
 
+import TickSvg from "@public/tick.svg";
+import SearchSvg from "@public/search.svg";
+
 import "./phone-number-input.css";
 
 export type PhoneNumberInputProps = {
@@ -20,17 +23,24 @@ const PhoneNumberInput = (props: PhoneNumberInputProps) => {
   const { id, label, required, onChange } = props;
 
   const [selectCountry, setSelectCountry] = useState(phoneNumberLocals[0]);
+  const [searchLocal, setSearchLocal] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [value, setValue] = useState("");
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const number = e.target.value;
+    const numRegex = /[^0-9]/g;
+    if (numRegex.test(number)) return;
     setValue(number);
     if (onChange) {
       onChange({ number, ...selectCountry });
     }
   };
+
+  const filterLocalsList = phoneNumberLocals.filter((local) => {
+    return local.label.toLowerCase().includes(searchLocal.toLowerCase());
+  });
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -47,16 +57,16 @@ const PhoneNumberInput = (props: PhoneNumberInputProps) => {
 
   return (
     <div ref={wrapperRef}>
-      <label
-        className="Phone-number-field"
-        // onClick={() => setIsOpen((prev) => !prev)}
-      >
+      <label className="Phone-number-field">
         <label htmlFor={id} className="Phone-number-field-label">
           {label} {required ? "*" : ""}
         </label>
         <div className="Phone-number-field-section">
-          <div
-            onClick={() => setIsOpen((prev) => !prev)}
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setIsOpen((prev) => !prev);
+            }}
             className="Phone-number-field-selected-flag"
           >
             <img
@@ -69,54 +79,71 @@ const PhoneNumberInput = (props: PhoneNumberInputProps) => {
               alt="chevron"
               className="Phone-number-field-chevron"
             />
-          </div>
+          </button>
           <input
             id={`${id}-region-code`}
             type="tel"
             className="Phone-number-field-region-code"
             readOnly
             placeholder={selectCountry.countryCode}
-            value={selectCountry.countryCode}
+            value={value ? selectCountry.countryCode : ""}
           />
 
           <input
             id={id}
             type="tel"
+            inputMode="numeric"
+            pattern="[0-9]*"
             className="Phone-number-field-input"
             value={value}
             onClick={() => setIsOpen(false)}
             onChange={handleOnChange}
+            placeholder="(____)-______"
           />
         </div>
-        <div
-          onClick={() => setIsOpen(true)}
-          className={`${isOpen ? "Phone-number-field-list" : "hide"}`}
-        >
-          {phoneNumberLocals?.map((country) => {
-            return (
-              <div
-                onClick={() => {
-                  setSelectCountry(country);
-                }}
-                className={`Phone-number-field-container ${
-                  country.code === selectCountry.code ? "selected-option" : ""
-                }`}
-                key={country.label}
-              >
-                <img
-                  src={country.flagImgSrc}
-                  alt="flag"
-                  className="Phone-number-flag"
-                />
-                <input
-                  className="Phone-number-field-item"
-                  id={selectCountry.code}
-                  type="button"
-                  value={country.label}
-                />
-              </div>
-            );
-          })}
+        <div className={`${isOpen ? "Phone-country-search" : "hide"}`}>
+          <div className="Phone-number-field-list-search-container">
+            <SearchSvg className="Phone-number-field-list-search-icon" />
+            <input
+              className="Phone-number-field-list-search"
+              placeholder="Search"
+              onChange={(e) => setSearchLocal(e.target.value)}
+            />
+          </div>
+          <div className="Phone-number-field-list">
+            {filterLocalsList?.map((country) => {
+              const selected = country.code === selectCountry.code;
+              return (
+                <div
+                  onClick={() => {
+                    setSelectCountry(country);
+                    setIsOpen(false);
+                  }}
+                  className={`Phone-number-field-container ${
+                    selected ? "selected-option" : ""
+                  }`}
+                  key={country.label}
+                >
+                  <img
+                    src={country.flagImgSrc}
+                    alt="flag"
+                    className="Phone-number-flag"
+                  />
+                  <input
+                    className="Phone-number-field-item"
+                    id={selectCountry.code}
+                    type="button"
+                    value={country.label}
+                    onClick={() => {
+                      setSelectCountry(country);
+                      setIsOpen(false);
+                    }}
+                  />
+                  {selected && <TickSvg className="Phone-number-field-tick" />}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </label>
     </div>
