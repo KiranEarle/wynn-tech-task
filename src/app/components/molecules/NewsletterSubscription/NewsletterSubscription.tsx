7203974fa6cca50238864757ba2d6ed8859905f9
KiranEarle <earlekiran@gmail.com>
2025-06-01 17:02:17 +0100

    updates to the newsletter component

--- src/app/components/molecules/NewsletterSubscription/NewsletterSubscription.tsx
+++ src/app/components/molecules/NewsletterSubscription/NewsletterSubscription.tsx
@@ -1,7 +1,42 @@
-import "./newsletter-subscription.css";
+"use client";
+
+import Text from "@components/atoms/Text";
+import Button from "@components/atoms/Button";
+
+import style from "./newsletter-subscription.module.css";
 
 const NewsletterSubscription = () => {
-  return <div className="Newsletter"></div>;
+  const onClick = (e) => {
+    console.log(e);
+  };
+  return (
+    <div className={style.Newsletter}>
+      <div className={style.Newsletter_container}>
+        <div
+          className={`${style.Newsletter_title} ${style.Newsletter_content}`}
+        >
+          <Text text="Get News & Updates" type="h2" priority="heading" />
+        </div>
+        <div className={`${style.Newsletter_text} ${style.Newsletter_content}`}>
+          <Text
+            text="Get latest developments and exciting news on how we are shaping the future!"
+            type="p"
+            priority="normal"
+          />
+        </div>
+        <div className={style.Newsletter_content}>
+          <form className={style.Newsletter_email}>
+            <input placeholder="Your email address" />
+            <Button
+              label="Join The Newsletter"
+              priority="secondary"
+              onClick={onClick}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewsletterSubscription;
